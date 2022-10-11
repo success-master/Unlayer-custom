8db6213a31501a5923adc31f92aa6fda2a29c3b8
success-master <55299139+success-master@users.noreply.github.com>
2022-10-11 10:06:57 -0500

    Update unlayer.js

--- unlayer.js
+++ unlayer.js
@@ -1,5 +1,21 @@
-function test() {
-    alert("Hello World!$");
-}
+stockHandle() {
+    switch (this.stockTitle) {
+      case 'Glossy':
+        console.log("Glossy");
+        (document.querySelector('.u_row') as HTMLElement).style.backgroundColor = 'red';
+        break;
 
-test();
+      case 'Paper':
+        console.log("Paper");
+        (document.querySelector('.u_row') as HTMLElement).style.backgroundColor = 'green';
+        break;
+
+      case 'Poly':
+        console.log("Poly");
+        (document.querySelector('.u_row') as HTMLElement).style.backgroundColor = 'blue';
+        break;
+
+      default:
+        break;
+    }
+}
