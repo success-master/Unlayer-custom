896fb7315152e8c635053bcc01045c96090779ed
success-master <55299139+success-master@users.noreply.github.com>
2022-10-11 10:44:39 -0500

    Update unlayer.js

--- unlayer.js
+++ unlayer.js
@@ -1,5 +1,5 @@
 var divsToHide = Array.from(
-  document.getElementsByClassName("u_row") as HTMLCollectionOf<HTMLElement>
+  (document.getElementsByClassName("u_row") as HTMLCollectionOf<HTMLElement>)
 );
 console.log("divsToHide: ", divsToHide);
 for (var i = 0; i < divsToHide.length; i++) {
