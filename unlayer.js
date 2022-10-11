56b16052f9a083302df2e7a9dae9228160f83808
success-master <55299139+success-master@users.noreply.github.com>
2022-10-11 10:42:02 -0500

    Update unlayer.js

--- unlayer.js
+++ unlayer.js
@@ -1,21 +1,8 @@
-stockHandle() {
-    switch (this.stockTitle) {
-      case 'Glossy':
-        console.log("Glossy");
-        (document.querySelector('.u_row') as HTMLElement).style.backgroundColor = 'red';
-        break;
+var divsToHide = Array.from(
+  document.getElementsByClassName("u_row") as HTMLCollectionOf<HTMLElement>
+);
+console.log("divsToHide: ", divsToHide);
+for (var i = 0; i < divsToHide.length; i++) {
 
-      case 'Paper':
-        console.log("Paper");
-        (document.querySelector('.u_row') as HTMLElement).style.backgroundColor = 'green';
-        break;
-
-      case 'Poly':
-        console.log("Poly");
-        (document.querySelector('.u_row') as HTMLElement).style.backgroundColor = 'blue';
-        break;
-
-      default:
-        break;
-    }
+    divsToHide[i].style.backgroundColor = "red";
 }
